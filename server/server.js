4a3ac5200cec4422c8a5cc596e659e0c98009486
adanxx <abdi0361@stud.kea.dk>
2017-11-15 13:20:08 +0100

    Add: Socket-lib  and connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 const path = require('path'); // the path-lib is build in, and dosen't require npm-download
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname + '/../public');
-const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname + '/../public'); // the path-lib is an easy extention to direct to files in different directive 
+const port = process.env.PORT || 3000; // our simple process path interchanges between the localport server and heroku server when it is online
+
 
 var app = express();
-app.use(express.static(publicPath));
+var server = http.createServer(app);
+var io = socketIO(server);
 
-app.listen(port, ()=>{
-  console.log('Server is runing on Port:'+ port);
+app.use(express.static(publicPath)); // we use a path-lib to direct the server to index.html page.
+
+
+io.on('connection', (socket) => {
+    console.log();
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log();
+        console.log('Client disconnected form the server');
+    });
+
+});
+
+
+
+server.listen(port, () => {
+
+    console.log('Server is runing on Port:' + port);
 })
 
 ///console.log(__dirname + '/../public');  //dirname reference the current directiv, in this case it "server-folder"
