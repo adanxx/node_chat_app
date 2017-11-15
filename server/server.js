5d1d8d2ab8a8d4a2ca1609d9d23049788a8b03b0
adanxx <abdi0361@stud.kea.dk>
2017-11-15 12:25:52 +0100

    Node-chat-app init

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path'); // the path-lib is build in, and dosen't require npm-download
+const express = require('express');
+
+const publicPath = path.join(__dirname + '/../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(publicPath));
+
+app.listen(port, ()=>{
+  console.log('Server running on Port:'+ port);
+})
+
+///console.log(__dirname + '/../public');  //dirname reference the current directiv, in this case it "server-folder"
+//console.log(publicPath);
