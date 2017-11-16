1d196efbfdfd92836e06f6c317ea81ce481fd50b
adanxx <abdi0361@stud.kea.dk>
2017-11-16 11:41:30 +0100

    Upload: Moment-lib

--- playground/time.js
+++ playground/time.js
@@ -1,6 +1,13 @@
 
 //browser Date().getTime();
 
+const moment = require('moment');
+
+/*
 var date  = new Date ()
 var months = ['jan', 'Feb'];
 console.log(date.getHours());
+*/
+var date = moment();
+
+console.log(date.format('MMMM Do, YYYY'));
